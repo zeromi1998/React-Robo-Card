11e85e965b180a76fe5cbe7c4d481daee4bffd01
Prathamesh98 <prathamesh98@localhost.localdomain>
2019-10-24 19:48:40 +0530

    Updates

---
+++ precache-manifest.cd54ac5e1a2bf453f4298228326db6ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a695165b9c666f99bd54bf609e8a7e",
+    "url": "/React-Robo-Card/index.html"
+  },
+  {
+    "revision": "4157ca1ae12645edb3dd",
+    "url": "/React-Robo-Card/static/css/main.220375ba.chunk.css"
+  },
+  {
+    "revision": "1b93be959a299ad27afe",
+    "url": "/React-Robo-Card/static/js/2.81680560.chunk.js"
+  },
+  {
+    "revision": "4157ca1ae12645edb3dd",
+    "url": "/React-Robo-Card/static/js/main.cf49f289.chunk.js"
+  },
+  {
+    "revision": "2083186cc57e6696a209",
+    "url": "/React-Robo-Card/static/js/runtime-main.08bde99d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Robo-Card/precache-manifest.cd54ac5e1a2bf453f4298228326db6ac.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Robo-Card/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
